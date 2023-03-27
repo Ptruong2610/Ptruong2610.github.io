a4a1b8136494962718bbc942758d8ff77f3f134b
Ptruong2610 <ptruong.dev@gmail.com>
2023-03-27 23:16:53 +0700

    javacore

--- javacore/src/Btvn.java
+++ javacore/src/Btvn.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Btvn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String again = "";
         do {
             System.out.print("Please input name student: ");
             String name = scanner.nextLine();
@@ -13,15 +14,12 @@ public class Btvn {
             System.out.println(" information of the student: " +"name:" +name+" address:" +address+ " age:" + age);
             System.out.print("Do you want to continues?(Y/N): ");
 
-            String again = scanner.nextLine();
+             again = scanner.nextLine();
 
             // Kiểm tra ký tự ngắt
             if (again.equalsIgnoreCase("n")) {
                 break;
-            }else if (again.equalsIgnoreCase("y")) {
-
-                System.out.println("Please enter student information ");
             }
-        } while (true);
+        } while (again.equalsIgnoreCase("y"));
     }
 }
