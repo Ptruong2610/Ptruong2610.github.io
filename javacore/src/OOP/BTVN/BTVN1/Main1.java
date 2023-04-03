0685dfee84b63c71d1bf0e46104a16c9b72ec5da
Ptruong2610 <ptruong.dev@gmail.com>
2023-04-03 17:49:43 +0700

    Exoop

--- javacore/src/OOP/BTVN/BTVN1/Main1.java
+++ javacore/src/OOP/BTVN/BTVN1/Main1.java
@@ -23,7 +23,7 @@ public class Main1 {
         System.out.println("Mời bạn nhập cạnh hình vuông:");
         square.setSide(sc.nextDouble());
         System.out.println("Chu vi hình chữ nhật là:"+rectangle.chuVi());
-        System.out.println("Diện tihcs hình chữ nhật là:"+rectangle.dienTich());
+        System.out.println("Diện tích hình chữ nhật là:"+rectangle.dienTich());
         System.out.println("Chu vi hình vuông là:"+square.chuVi());
         System.out.println("Diện tích hình vuông là:"+square.dienTich());
         System.out.println(rectangle);
