77a4fbe2e244cc612cfd6f4fd8b677afd1aee6a3
Ptruong2610 <ptruong.dev@gmail.com>
2023-04-07 23:54:06 +0700

    Lap2

--- javacore/src/main.java
+++ javacore/src/main.java
@@ -10,16 +10,31 @@ public class main {
         Scanner scanner = new Scanner(System.in);
 //        Menu menu = new Menu();
 //        menu.selectType(scanner);
-        System.out.println("Mời bạn nhập số lượng student muốn tạo:");
-        int n = Integer.parseInt(scanner.nextLine());
+        int n;
+        do {
+            System.out.println("Mời bạn nhập số lượng student muốn tạo:");
+             n = Integer.parseInt(scanner.nextLine());
+             if (n<0)
+                 System.out.println("Bạn phải tạo số lượng sinh viên muốn nhập phải lớn hơn 0");
+        }while (n<0);
 
         Student[] students = new Student[n];
         StudentHandle studentHandle = new StudentHandle();
         for (int i = 0; i < n; i++) {
             //Creat student
-            Student student = studentHandle.inputStudent(scanner, i);
-            //Lưu student vào trong array;
-            students[i] = student;
+            do {
+                Student student = studentHandle.inputStudent(scanner, i);
+                //Lưu student vào trong array;
+                students[i] = student;
+                if (students[i].getScoreMath()<0||students[i].getScoreMath()>10||students[i].getScourePhysic()<0
+                        ||students[i].getScourePhysic()>10||students[i].getScoureChemistry()<0||students[i].getScoureChemistry()>10){
+                    System.out.println("Bạn phải nhập điểm >0 và <10.Xin mời nhập lại:");
+                }
+            }while (students[i].getScoreMath()<0||students[i].getScoreMath()>10||students[i].getScourePhysic()<0
+                    ||students[i].getScourePhysic()>10||students[i].getScoureChemistry()<0||students[i].getScoureChemistry()>10);
+//            Student student = studentHandle.inputStudent(scanner, i);
+//            //Lưu student vào trong array;
+//            students[i] = student;
         }
         //Display student form students[]
         studentHandle.displayStudents(students,n);
