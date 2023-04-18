fafafd58ff7473f813c6c383814f0d160e9c3b7c
Ptruong2610 <ptruong.dev@gmail.com>
2023-04-18 12:39:35 +0700

    Login - Logout - Register_2

--- javacore/src/TestOOP/view/Menu.java
+++ javacore/src/TestOOP/view/Menu.java
@@ -28,75 +28,91 @@ public class Menu {
     public void selectMenu(Scanner scanner, ArrayList<User> users){
         UserHandle userHandle = new UserHandle();
         showMenu();
-        int option = Integer.parseInt(scanner.nextLine());
+        try {
+            int option = Integer.parseInt(scanner.nextLine());
 
-        switch (option){
-            case 1:
-                userHandle.signInUser(scanner,users);
-                break;
-            case 2:
-                userHandle.addUser(scanner,users);
-                System.out.println("Tạo tài khoản tài khoản thành công");
-                selectMenu(scanner,users);
-                break;
-            default:
-                System.out.println("Chọn lại!!!");
-                selectMenu(scanner,users);
-                break;
+            switch (option){
+                case 1:
+                    userHandle.signInUser(scanner,users);
+                    break;
+                case 2:
+                    userHandle.addUser(scanner,users);
+                    System.out.println("Tạo tài khoản tài khoản thành công");
+                    selectMenu(scanner,users);
+                    break;
+                default:
+                    System.out.println("Chọn lại!!!");
+                    selectMenu(scanner,users);
+                    break;
+            }
+
+        }catch (NumberFormatException  e){
+            System.out.println("Bạn phải nhập vào số");
         }
+        selectMenu(scanner,users);
     }
     public void menuLogin(Scanner scanner, ArrayList<User> users,String userName){
         UserHandle userHandle = new UserHandle();
             showMenuLogin();
-            int option = Integer.parseInt(scanner.nextLine());
-            switch (option){
-                case 1:
-                    User userEditUserName = userHandle.checkByUserName(users,userName);
-                    userHandle.editUserName(scanner,users,userEditUserName);
-                    break;
-                case 2:
-                    User userEditEmail= userHandle.checkByUserName(users,userName);
-                    userHandle.editEmail(scanner,users,userEditEmail);
-                    System.out.println("Đổi Email thành công!Mời bạn tiếp tục công việc");
-                    menuLogin(scanner,users,userName);
-                    break;
-                case 3:
-                    User userEditPassword = userHandle.checkByUserName(users,userName);
-                    userHandle.editPassword(scanner,users,userEditPassword);
-                    break;
-                case 4:
-                    selectMenu(scanner, users);
-                    break;
-                case 5:
-                    System.exit(0);
-                    break;
+            try {
+                int option = Integer.parseInt(scanner.nextLine());
+                switch (option){
+                    case 1:
+                        User userEditUserName = userHandle.checkByUserName(users,userName);
+                        userHandle.editUserName(scanner,users,userEditUserName);
+                        break;
+                    case 2:
+                        User userEditEmail= userHandle.checkByUserName(users,userName);
+                        userHandle.editEmail(scanner,users,userEditEmail);
+                        System.out.println("Đổi Email thành công!Mời bạn tiếp tục công việc");
+                        menuLogin(scanner,users,userName);
+                        break;
+                    case 3:
+                        User userEditPassword = userHandle.checkByUserName(users,userName);
+                        userHandle.editPassword(scanner,users,userEditPassword);
+                        break;
+                    case 4:
+                        selectMenu(scanner, users);
+                        break;
+                    case 5:
+                        System.exit(0);
+                        break;
 
-                default:
-                    System.out.println("Chọn lại!!!");
-                    menuLogin(scanner,users,userName);
-                    break;
+                    default:
+                        System.out.println("Chọn lại!!!");
+                        menuLogin(scanner,users,userName);
+                        break;
+                }
+            }catch (NumberFormatException  e) {
+                System.out.println("Lỗi!Bạn phải nhập vào số");
             }
+        menuLogin(scanner,users,userName);
     }
 
     public void menuErrPassword(Scanner scanner,ArrayList<User> users,String userName){
         UserHandle userHandle = new UserHandle();
         showMenuErrPassword();
-        int option = Integer.parseInt(scanner.nextLine());
-        switch (option){
-            case 1:
-//                selectMenu(scanner,users);
-                userHandle.signInUser(scanner,users);
-                System.out.println();
-                break;
-            case 2:
-                User userSearchEmail = userHandle.checkByUserName(users,userName);
-                userHandle.searchEmailUpdatePassword(scanner,userSearchEmail,users);
-                break;
-            default:
-                System.out.println("Chọn lại: 1 or 2 !!!");
-                menuErrPassword(scanner,users,userName);
-                break;
+        try {
+            int option = Integer.parseInt(scanner.nextLine());
+            switch (option){
+                case 1:
+                    userHandle.signInUser(scanner,users);
+                    System.out.println();
+                    break;
+                case 2:
+                    User userSearchEmail = userHandle.checkByUserName(users,userName);
+                    userHandle.searchEmailUpdatePassword(scanner,userSearchEmail,users);
+                    break;
+                default:
+                    System.out.println("Chọn lại: 1 or 2 !!!");
+                    menuErrPassword(scanner,users,userName);
+                    break;
+            }
+        }catch (NumberFormatException  e) {
+            System.out.println("Lỗi!Bạn phải nhập vào số");
         }
+        menuErrPassword(scanner,users,userName);
+
     }
 
 
